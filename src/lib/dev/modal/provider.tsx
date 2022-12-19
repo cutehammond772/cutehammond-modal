bc20d0e73f1fefc09ecb7760a8bf32ea16250f62
LEE\JHL <cutehammond772@gmail.com>
2022-12-20 06:10:01 +0900

    :sparkles: feat: createModal 함수 추가

---
+++ src/lib/dev/modal/provider.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { AppProvider } from "../../redux";
+import ModalContainer from "./container";
+
+const GlobalModalProvider = (props: React.PropsWithChildren) => (
+  <AppProvider>
+    <ModalContainer>{props.children}</ModalContainer>
+  </AppProvider>
+);
+
+export default React.memo(GlobalModalProvider);
